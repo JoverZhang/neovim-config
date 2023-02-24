bda9e91d16ac8e9a91ce2898e518f622de82ab57
JoverZhang <joverzh@gmail.com>
2023-02-25 00:00:10 +0800

    autocmd: set textwidth for languages

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,21 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local language_textwidths = {
+  c = 80,
+  cpp = 120,
+  go = 120,
+  lua = 120,
+  python = 80,
+  rust = 120,
+}
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  callback = function(opts)
+    local textwidth = language_textwidths[vim.bo[opts.buf].filetype]
+    if textwidth then
+      vim.cmd("set textwidth=" .. textwidth)
+    end
+  end,
+})
