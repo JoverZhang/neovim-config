feb02264761a4edacbeb1b1146db824d3da2fb8b
JoverZhang <joverzh@gmail.com>
2023-02-27 16:43:06 +0800

    Add golang lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,6 +6,9 @@ return {
       ensure_installed = {
         "clangd",
         "cmakelang",
+        "golangci-lint",
+        "goimports",
+        "gopls",
         "rust-analyzer",
         "stylua",
         "shellcheck",
@@ -96,5 +99,32 @@ return {
         })
       end,
     },
+  },
+
+  -- golang
+  {
+
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup({
+        goimport = "gopls", -- if set to 'gopls' will use golsp format
+        gofmt = "gopls", -- if set to gopls will use golsp format
+        max_line_len = 120,
+        tag_transform = false,
+        test_dir = "",
+        comment_placeholder = "   ",
+        lsp_cfg = true, -- false: use your own lspconfig
+        lsp_gofumpt = true, -- true: set default gofmt in gopls format to gofumpt
+        lsp_on_attach = true, -- use on_attach from go.nvim
+      })
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
 }
