520c90a20ad8df70b8b88609b79e1e626b6fd4aa
JoverZhang <joverzh@gmail.com>
2023-02-27 16:43:16 +0800

    Add TreeSister config

---
+++ lua/plugins/treesister.lua
@@ -0,0 +1,37 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    highlight = { enable = true },
+    indent = { enable = true },
+    context_commentstring = { enable = true, enable_autocmd = false },
+    ensure_installed = {
+      "bash",
+      "cpp",
+      "go",
+      "help",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "rust",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = "<nop>",
+        node_decremental = "<bs>",
+      },
+    },
+  },
+}
