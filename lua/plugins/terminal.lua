821782a7a7a94384405734f855c3a2f9e50c2921
JoverZhang <joverzh@gmail.com>
2023-03-04 18:22:25 +0800

    Update toggleterm.nvim keymaps

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -7,7 +7,6 @@ return {
       vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
       vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
       vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-      vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
     end
 
     -- if you only want these mappings for toggle term use term://*toggleterm#* instead
@@ -16,11 +15,11 @@ return {
   opts = {},
   keys = {
     { "<leader>th", "<cmd>ToggleTermToggleAll<cr>", desc = "ToggleTermToggleAll" },
-    { "<leader>ts", "<cmd>ToggleTerm 1 size=10 direction=horizontal<cr>", desc = "ToggleTerm 1" },
-    { "<leader>t1", "<cmd>ToggleTerm 1 size=10 direction=horizontal<cr>", desc = "ToggleTerm 1" },
-    { "<leader>t2", "<cmd>ToggleTerm 2 size=10 direction=horizontal<cr>", desc = "ToggleTerm 2" },
-    { "<leader>t3", "<cmd>ToggleTerm 3 size=10 direction=horizontal<cr>", desc = "ToggleTerm 3" },
-    { "<leader>tv", "<cmd>ToggleTerm 10 size=40 direction=vertical<cr>", desc = "ToggleTerm vertical 10" },
+    { "<leader>ts", "<cmd>ToggleTerm 1 direction=horizontal<cr>", desc = "ToggleTerm horizontal" },
+    { "<leader>t1", "<cmd>ToggleTerm 1 direction=horizontal<cr>", desc = "ToggleTerm 1" },
+    { "<leader>t2", "<cmd>ToggleTerm 2 direction=horizontal<cr>", desc = "ToggleTerm 2" },
+    { "<leader>t3", "<cmd>ToggleTerm 3 direction=horizontal<cr>", desc = "ToggleTerm 3" },
+    { "<leader>tv", "<cmd>ToggleTerm 10 direction=vertical<cr>", desc = "ToggleTerm vertical 10" },
     { "<leader>tf", "<cmd>ToggleTerm 11 direction=float<cr>", desc = "ToggleTerm float 11" },
   },
 }
