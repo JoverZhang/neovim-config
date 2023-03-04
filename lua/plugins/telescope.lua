a0adad511c9ec8161039c96fb7082ed806af4975
JoverZhang <joverzh@gmail.com>
2023-03-04 23:10:45 +0800

    Update telescope keymappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -89,36 +89,36 @@ return {
       defaults = {
         prompt_prefix = " ",
         selection_caret = " ",
-        -- mappings = {
-        --   i = {
-        --     ["<c-t>"] = function(...)
-        --       return require("trouble.providers.telescope").open_with_trouble(...)
-        --     end,
-        --     ["<a-i>"] = function()
-        --       Util.telescope("find_files", { no_ignore = true })()
-        --     end,
-        --     ["<a-h>"] = function()
-        --       Util.telescope("find_files", { hidden = true })()
-        --     end,
-        --     ["<C-Down>"] = function(...)
-        --       return require("telescope.actions").cycle_history_next(...)
-        --     end,
-        --     ["<C-Up>"] = function(...)
-        --       return require("telescope.actions").cycle_history_prev(...)
-        --     end,
-        --     ["<C-f>"] = function(...)
-        --       return require("telescope.actions").preview_scrolling_down(...)
-        --     end,
-        --     ["<C-b>"] = function(...)
-        --       return require("telescope.actions").preview_scrolling_up(...)
-        --     end,
-        --   },
-        --   n = {
-        --     ["q"] = function(...)
-        --       return require("telescope.actions").close(...)
-        --     end,
-        --   },
-        -- },
+        mappings = {
+          i = {
+            ["<c-t>"] = function(...)
+              return require("trouble.providers.telescope").open_with_trouble(...)
+            end,
+            -- ["<a-i>"] = function()
+            --   Util.telescope("find_files", { no_ignore = true })()
+            -- end,
+            -- ["<a-h>"] = function()
+            --   Util.telescope("find_files", { hidden = true })()
+            -- end,
+            ["<C-Down>"] = function(...)
+              return require("telescope.actions").cycle_history_next(...)
+            end,
+            ["<C-Up>"] = function(...)
+              return require("telescope.actions").cycle_history_prev(...)
+            end,
+            -- ["<C-f>"] = function(...)
+            --   return require("telescope.actions").preview_scrolling_down(...)
+            -- end,
+            -- ["<C-b>"] = function(...)
+            --   return require("telescope.actions").preview_scrolling_up(...)
+            -- end,
+          },
+          n = {
+            ["q"] = function(...)
+              return require("telescope.actions").close(...)
+            end,
+          },
+        },
       },
       extensions = {
         fzf = {
