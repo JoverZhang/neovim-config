a1d8dc799221a8133cfa65796eb751cd039743f2
JoverZhang <joverzh@gmail.com>
2023-07-04 18:47:45 +0800

    NeoVide supports

---
+++ lua/config/neovide.lua
@@ -0,0 +1,7 @@
+if vim.g.neovide then
+  vim.o.guifont = "JetBrains Mono:h11" -- text below applies for VimScript
+  vim.g.neovide_transparency = 1
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_profiler = false
+  vim.g.neovide_refresh_rate = 90
+end
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -7,8 +7,15 @@ return {
       "<leader>ut",
       function()
         local tokyonight = require("tokyonight")
+        -- opaque
         if t then
           tokyonight.setup()
+          -- neovide
+          if vim.g.neovide then
+            vim.g.neovide_transparency = 1
+          end
+
+        -- transparent
         else
           tokyonight.setup({
             transparent = true,
@@ -17,6 +24,10 @@ return {
               floats = "transparent",
             },
           })
+          -- neovide
+          if vim.g.neovide then
+            vim.g.neovide_transparency = 0.8
+          end
         end
 
         t = not t
