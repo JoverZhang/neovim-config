e51b07726dd010f76dc26159e1445159cec8b4f3
JoverZhang <joverzh@gmail.com>
2023-07-05 12:27:04 +0800

    Update dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -16,7 +16,8 @@ return {
     dashboard.section.buttons.val = {
       dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
       dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+      dashboard.button("r", "Se" .. " Recent Session", [[:lua require("plugins.exts.sessions").load_recent() <cr>]]),
+      dashboard.button("R", " " .. " Recent files", ":Telescope oldfiles <CR>"),
       dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
       dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
       dashboard.button("s", "勒" .. " Restore Session", [[:lua require("plugins.exts.sessions").load() <cr>]]),
