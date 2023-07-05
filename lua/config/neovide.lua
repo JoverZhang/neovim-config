692428612ee7b3cd1c7159c2776c85ca05776d00
JoverZhang <joverzh@gmail.com>
2023-07-05 13:13:07 +0800

    NeoVide: supports clipboard

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -4,4 +4,7 @@ if vim.g.neovide then
   vim.g.neovide_remember_window_size = true
   vim.g.neovide_profiler = false
   vim.g.neovide_refresh_rate = 90
+
+  vim.api.nvim_set_keymap("i", "<c-s-v>", "<c-r>+", { noremap = true })
+  vim.api.nvim_set_keymap("c", "<c-s-v>", "<c-r>+", { noremap = true })
 end
