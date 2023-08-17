4c9f4f8f03c8aacae2f1b9c8eefb9e5094d18077
JoverZhang <joverzh@gmail.com>
2023-08-17 11:39:41 +0800

    fix: GoLang duplicate Inlay-Hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,7 +9,6 @@ return {
         "cmakelang",
         "golangci-lint",
         "goimports",
-        "gopls",
         "rust-analyzer",
         "stylua",
         "shellcheck",
@@ -33,6 +32,7 @@ return {
       { "<leader>c]", vim.diagnostic.goto_next, desc = "goto next diagnostic" },
     },
   },
+
   {
     "lvimuser/lsp-inlayhints.nvim",
     branch = "anticonceal",
@@ -98,6 +98,13 @@ return {
 
           local bufnr = args.buf
           local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+          -- disable inlay hints for golang
+          local filetype = vim.fn.getbufvar(bufnr, "&filetype")
+          if filetype == "go" then
+            return
+          end
+
           require("lsp-inlayhints").on_attach(client, bufnr)
           vim.diagnostic.config({
             severity_sort = true,
@@ -335,33 +342,47 @@ return {
 
   -- golang
   {
-    "ray-x/go.nvim",
-    dependencies = { -- optional packages
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "ray-x/go.nvim",
+        init = function()
+          require("go").setup({
+            goimport = "gopls", -- if set to 'gopls' will use golsp format
+            gofmt = "gopls", -- if set to gopls will use golsp format
+            max_line_len = 120,
+            tag_transform = false,
+            test_dir = "",
+            comment_placeholder = "   ",
+            lsp_cfg = true, -- false: use your own lspconfig
+            -- TODO: fix go.nvim keymaps confiect
+            lsp_keymaps = true, -- true: use default keymaps defined in go/lsp.lua
+            lsp_inlay_hints = {
+              other_hints_prefix = "",
+            },
+            lsp_gofumpt = true, -- true: set default gofmt in gopls format to gofumpt
+            lsp_on_attach = true, -- use on_attach from go.nvim
+          })
+        end,
+      },
+      {
+        "ray-x/guihua.lua",
+      },
     },
-    config = function()
-      require("go").setup({
-        goimport = "gopls", -- if set to 'gopls' will use golsp format
-        gofmt = "gopls", -- if set to gopls will use golsp format
-        max_line_len = 120,
-        tag_transform = false,
-        test_dir = "",
-        comment_placeholder = "   ",
-        lsp_cfg = true, -- false: use your own lspconfig
-        -- TODO: fix go.nvim keymaps confiect
-        lsp_keymaps = true, -- true: use default keymaps defined in go/lsp.lua
-        lsp_inlay_hints = {
-          other_hints_prefix = "",
+    opts = {
+      servers = {
+        gopls = {
+          hints = {
+            assignVariableTypes = true,
+            compositeLiteralFields = true,
+            constantValues = true,
+            functionTypeParameters = true,
+            parameterNames = true,
+            rangeVariableTypes = true,
+          },
         },
-        lsp_gofumpt = true, -- true: set default gofmt in gopls format to gofumpt
-        lsp_on_attach = true, -- use on_attach from go.nvim
-      })
-    end,
-    event = { "CmdlineEnter" },
-    ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+      },
+    },
   },
 
   -- Java
